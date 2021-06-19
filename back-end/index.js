a3c73451ce6e605ea27eb2d380b7b12612575b64
Fernando Antunes <fernando.antunes1@gmail.com>
2021-06-19 17:20:12 -0300

    implementado arquivo .env com variaveis de ambiente pra conectar ao banco de dados e configuração do arquivo config do sequelize

--- back-end/index.js
+++ back-end/index.js
@@ -1,10 +1,11 @@
 const express = require('express');
 const cors = require('cors');
+require("dotenv").config();
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
 
-const PORT = 3001;
+const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => console.log(`Rodando na porta ${PORT}!`));
