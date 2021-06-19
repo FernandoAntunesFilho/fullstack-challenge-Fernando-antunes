e57d9f2861daa0ea3b575fb6e2c8aaf726e09e74
Fernando Antunes <fernando.antunes1@gmail.com>
2021-06-19 17:02:16 -0300

    primeiro commit | preparação do ambiente de back end

---
+++ back-end/index.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const cors = require('cors');
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+const PORT = 3001;
+app.listen(PORT, () => console.log(`Rodando na porta ${PORT}!`));
